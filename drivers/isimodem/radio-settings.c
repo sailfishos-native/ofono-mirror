sailfishos-native/ofono-mirror

commit 469ae3ba523c210a2e1dfa2925d3fdb929b2d5c7

radio-settings: Port drivers to use OFONO_ATOM_DRIVER_BUILTIN

drivers/isimodem/radio-settings.c
@@ -37,7 +37,6 @@
 #include <ofono/modem.h>
 #include <ofono/radio-settings.h>
 
-#include "isimodem.h"
 #include "isiutil.h"
 #include "debug.h"
 #include "gpds.h"
@@ -364,7 +363,6 @@ static void isi_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= "isimodem",
 	.probe			= isi_radio_settings_probe,
 	.remove			= isi_radio_settings_remove,
 	.query_rat_mode		= isi_query_rat_mode,
@@ -373,12 +371,4 @@ static const struct ofono_radio_settings_driver driver = {
 	.set_fast_dormancy	= isi_set_fast_dormancy,
 };
 
-void isi_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void isi_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, isidriver, &driver)

drivers/gemaltomodem/radio-settings.c
@@ -35,10 +35,10 @@
 #include <ofono/modem.h>
 #include <ofono/radio-settings.h>
 
+#include <drivers/atmodem/atutil.h>
+
 #include "gatchat.h"
 #include "gatresult.h"
-
-#include "gemaltomodem.h"
 
 static const char *none_prefix[] = { NULL };
 static const char *sxrat_prefix[] = { "^SXRAT:", NULL };
@@ -246,19 +246,10 @@ static void gemalto_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= "gemaltomodem",
 	.probe			= gemalto_radio_settings_probe,
 	.remove			= gemalto_radio_settings_remove,
 	.query_rat_mode		= gemalto_query_rat_mode,
 	.set_rat_mode		= gemalto_set_rat_mode
 };
 
-void gemalto_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void gemalto_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, gemaltomodem, &driver)

drivers/hsomodem/radio-settings.c
@@ -34,10 +34,10 @@
 #include <ofono/modem.h>
 #include <ofono/radio-settings.h>
 
+#include <drivers/atmodem/atutil.h>
+
 #include "gatchat.h"
 #include "gatresult.h"
-
-#include "hsomodem.h"
 
 static const char *none_prefix[] = { NULL };
 static const char *opsys_prefix[] = { "_OPSYS:", NULL };
@@ -199,19 +199,10 @@ static void hso_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= "hsomodem",
 	.probe			= hso_radio_settings_probe,
 	.remove			= hso_radio_settings_remove,
 	.query_rat_mode		= hso_query_rat_mode,
 	.set_rat_mode		= hso_set_rat_mode
 };
 
-void hso_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void hso_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, hsomodem, &driver)

drivers/huaweimodem/radio-settings.c
@@ -34,10 +34,10 @@
 #include <ofono/modem.h>
 #include <ofono/radio-settings.h>
 
+#include <drivers/atmodem/atutil.h>
+
 #include "gatchat.h"
 #include "gatresult.h"
-
-#include "huaweimodem.h"
 
 static const char *none_prefix[] = { NULL };
 static const char *syscfg_prefix[] = { "^SYSCFG:", NULL };
@@ -583,7 +583,6 @@ static void huawei_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= "huaweimodem",
 	.probe			= huawei_radio_settings_probe,
 	.remove			= huawei_radio_settings_remove,
 	.query_rat_mode		= huawei_query_rat_mode,
@@ -592,12 +591,4 @@ static const struct ofono_radio_settings_driver driver = {
 	.set_band		= huawei_set_band,
 };
 
-void huawei_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void huawei_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, huaweimodem, &driver)

drivers/iceramodem/radio-settings.c
@@ -34,10 +34,10 @@
 #include <ofono/modem.h>
 #include <ofono/radio-settings.h>
 
+#include <drivers/atmodem/atutil.h>
+
 #include "gatchat.h"
 #include "gatresult.h"
-
-#include "iceramodem.h"
 
 static const char *none_prefix[] = { NULL };
 static const char *ipbm_prefix[] = { "%IPBM:", NULL };
@@ -212,19 +212,10 @@ static void icera_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= "iceramodem",
 	.probe			= icera_radio_settings_probe,
 	.remove			= icera_radio_settings_remove,
 	.query_rat_mode		= icera_query_rat_mode,
 	.set_rat_mode		= icera_set_rat_mode
 };
 
-void icera_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void icera_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, iceramodem, &driver)

drivers/ifxmodem/radio-settings.c
@@ -34,10 +34,10 @@
 #include <ofono/modem.h>
 #include <ofono/radio-settings.h>
 
+#include <drivers/atmodem/atutil.h>
+
 #include "gatchat.h"
 #include "gatresult.h"
-
-#include "ifxmodem.h"
 
 static const char *none_prefix[] = { NULL };
 static const char *xrat_prefix[] = { "+XRAT:", NULL };
@@ -200,19 +200,10 @@ static void ifx_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= "ifxmodem",
 	.probe			= ifx_radio_settings_probe,
 	.remove			= ifx_radio_settings_remove,
 	.query_rat_mode		= ifx_query_rat_mode,
 	.set_rat_mode		= ifx_set_rat_mode
 };
 
-void ifx_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void ifx_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, ifxmodem, &driver)

drivers/nwmodem/radio-settings.c
@@ -34,10 +34,10 @@
 #include <ofono/modem.h>
 #include <ofono/radio-settings.h>
 
+#include <drivers/atmodem/atutil.h>
+
 #include "gatchat.h"
 #include "gatresult.h"
-
-#include "nwmodem.h"
 
 static const char *none_prefix[] = { NULL };
 static const char *nwrat_prefix[] = { "$NWRAT:", NULL };
@@ -194,19 +194,10 @@ static void nw_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name			= "nwmodem",
 	.probe			= nw_radio_settings_probe,
 	.remove			= nw_radio_settings_remove,
 	.query_rat_mode		= nw_query_rat_mode,
 	.set_rat_mode		= nw_set_rat_mode
 };
 
-void nw_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void nw_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, nwmodem, &driver)

drivers/qmimodem/radio-settings.c
@@ -30,8 +30,7 @@
 #include "qmi.h"
 #include "nas.h"
 #include "dms.h"
-
-#include "qmimodem.h"
+#include "util.h"
 
 struct settings_data {
 	struct qmi_service *nas;
@@ -285,7 +284,6 @@ static void qmi_radio_settings_remove(struct ofono_radio_settings *rs)
 }
 
 static const struct ofono_radio_settings_driver driver = {
-	.name		= "qmimodem",
 	.probe		= qmi_radio_settings_probe,
 	.remove		= qmi_radio_settings_remove,
 	.set_rat_mode	= qmi_set_rat_mode,
@@ -293,12 +291,4 @@ static const struct ofono_radio_settings_driver driver = {
 	.query_available_rats = qmi_query_available_rats,
 };
 
-void qmi_radio_settings_init(void)
-{
-	ofono_radio_settings_driver_register(&driver);
-}
-
-void qmi_radio_settings_exit(void)
-{
-	ofono_radio_settings_driver_unregister(&driver);
-}
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, qmimodem, &driver)

commit 02b8710eff8a5bc7cf88ed0a1af73e08b96ce3be

isimodem: radio-settings: fix typo
Fixes: 469ae3ba ("radio-settings: Port drivers to use
OFONO_ATOM_DRIVER_BUILTIN")

drivers/isimodem/radio-settings.c
@@ -371,4 +371,4 @@ static const struct ofono_radio_settings_driver driver = {
 	.set_fast_dormancy	= isi_set_fast_dormancy,
 };
 
-OFONO_ATOM_DRIVER_BUILTIN(radio_settings, isidriver, &driver)
+OFONO_ATOM_DRIVER_BUILTIN(radio_settings, isimodem, &driver)