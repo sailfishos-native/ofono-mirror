sailfishos-native/ofono-mirror

commit fa3ec328b8afb296310d06142ac2cab61818cd12

stktest: Return non-0 exit code if tests failed

tools/stktest.c
@@ -4393,7 +4393,7 @@ static void test_destroy(gpointer user_data)
 	g_free(test);
 }
 
-static void __stktest_test_summarize(void)
+static int __stktest_test_summarize(void)
 {
 	GList *l;
 	unsigned int not_run = 0;
@@ -4428,6 +4428,8 @@ static void __stktest_test_summarize(void)
 			not_run + passed + failed, passed,
 			(float) passed * 100 / (not_run + passed + failed),
 			failed, not_run);
+
+	return !failed;
 }
 
 static void __stktest_test_cleanup(void)
@@ -4452,6 +4454,7 @@ int main(int argc, char **argv)
 	DBusError err;
 	guint watch;
 	struct sigaction sa;
+	int ret;
 
 	context = g_option_context_new(NULL);
 	g_option_context_add_main_entries(context, options, NULL);
@@ -4513,8 +4516,8 @@ int main(int argc, char **argv)
 
 	g_main_loop_unref(main_loop);
 
-	__stktest_test_summarize();
+	ret = __stktest_test_summarize();
 	__stktest_test_cleanup();
 
-	return 0;
+	return ret;
 }

commit a53878cc2d414960cbbd276fde5de212b2a320fa

stktest: Return number of failed tests
Fixes: fa3ec328 ("stktest: Return non-0 exit code if tests failed")

tools/stktest.c
@@ -4429,7 +4429,7 @@ static int __stktest_test_summarize(void)
 			(float) passed * 100 / (not_run + passed + failed),
 			failed, not_run);
 
-	return !failed;
+	return failed;
 }
 
 static void __stktest_test_cleanup(void)