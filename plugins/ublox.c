9c45043f652b02f2bc8d996f7041f50b5e3ff212
Denis Kenzior <denkenz@gmail.com>
2024-03-21 17:06:04 -0500

    ublox: Set modem lte capable

--- plugins/ublox.c
+++ plugins/ublox.c
@@ -91,6 +91,7 @@ static int ublox_probe(struct ofono_modem *modem)
 		return -ENOMEM;
 
 	ofono_modem_set_data(modem, data);
+	ofono_modem_set_capabilities(modem, OFONO_MODEM_CAPABILITY_LTE);
 
 	return 0;
 }
