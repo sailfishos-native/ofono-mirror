7fb17b7996de695ce09aee576ff5a189a1d5573d
Denis Kenzior <denkenz@gmail.com>
2024-06-03 10:37:39 -0500

    drivers: Use the new license header format

--- drivers/telitmodem/gprs-context-ncm.c
+++ drivers/telitmodem/gprs-context-ncm.c
@@ -1,18 +1,8 @@
 /*
+ * oFono - Open Source Telephony
+ * Copyright (C) 2017 Piotr Haber
  *
- *  oFono - Open Source Telephony
- *
- *  Copyright (C) 2017 Piotr Haber. All rights reserved.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
+ * SPDX-License-Identifier: GPL-2.0-only
  */
 
 #ifdef HAVE_CONFIG_H
