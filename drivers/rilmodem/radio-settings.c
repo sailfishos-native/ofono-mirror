ac1ff65c9808612094c2f61c09654cc62e0e7ef0
Denis Kenzior <denkenz@gmail.com>
2024-06-03 10:16:50 -0500

    rilmodem: Use the new license header format

--- drivers/rilmodem/radio-settings.c
+++ drivers/rilmodem/radio-settings.c
@@ -1,25 +1,10 @@
 /*
+ * oFono - Open Source Telephony
+ * Copyright (C) 2008-2011  Intel Corporation
+ * Copyright (C) 2013 Jolla Lt
+ * Copyright (C) 2014 Canonical Lt
  *
- *  oFono - Open Source Telephony
- *
- *  Copyright (C) 2008-2011  Intel Corporation. All rights reserved.
- *  Copyright (C) 2013 Jolla Ltd
- *  Contact: Jussi Kangas <jussi.kangas@tieto.com>
- *  Copyright (C) 2014 Canonical Ltd
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * SPDX-License-Identifier: GPL-2.0-only
  */
 #ifdef HAVE_CONFIG_H
 #include <config.h>
