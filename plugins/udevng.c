3071e0720e8e3505ce67159d318ad61c2ecbfa59
Steve Schrock <steve.schrock@getcruise.com>
2024-07-01 15:25:50 +0000

    udevng: Remove Diag setting from setup_gobi
    gobi does not use the Diag setting.

--- plugins/udevng.c
+++ plugins/udevng.c
@@ -356,7 +356,6 @@ static gboolean setup_gobi(struct modem_info *modem)
 	const struct device_info *qmi = NULL;
 	const struct device_info *net = NULL;
 	const char *gps = NULL;
-	const char *diag = NULL;
 	GSList *list;
 
 	DBG("%s", modem->syspath);
@@ -376,11 +375,7 @@ static gboolean setup_gobi(struct modem_info *modem)
 			net = info;
 		else if (g_strcmp0(subsystem, "tty") == 0) {
 			if (g_strcmp0(info->interface, "255/255/255") == 0) {
-				if (g_strcmp0(info->number, "00") == 0)
-					diag = info->devnode; /* ec20 */
-				else if (g_strcmp0(info->number, "01") == 0)
-					diag = info->devnode; /* gobi */
-				else if (g_strcmp0(info->number, "03") == 0)
+				if (g_strcmp0(info->number, "03") == 0)
 					gps = info->devnode; /* gobi */
 			} else if (g_strcmp0(info->interface, "255/0/0") == 0) {
 				if (g_strcmp0(info->number, "01") == 0)
@@ -393,13 +388,11 @@ static gboolean setup_gobi(struct modem_info *modem)
 	if (qmi == NULL || net == NULL)
 		return FALSE;
 
-	DBG("qmi=%s net=%s gps=%s diag=%s",
-			qmi->devnode, get_ifname(net), gps, diag);
+	DBG("qmi=%s net=%s gps=%s",
+			qmi->devnode, get_ifname(net), gps);
 
 	if (setup_qmi_qmux(modem, qmi, net) < 0)
 		return FALSE;
-
-	ofono_modem_set_string(modem->modem, "Diag", diag);
 
 	return TRUE;
 }
