5677ffdbe643f279fa03ba9acd4dd4849287747f
Denis Kenzior <denkenz@gmail.com>
2024-08-22 22:19:32 -0500

    qmi: wda: Convert #defines to an enum

--- drivers/qmimodem/wda.h
+++ drivers/qmimodem/wda.h
@@ -10,6 +10,9 @@
 
 /* Get and set data format interface */
 #define QMI_WDA_LL_PROTOCOL	0x11	/* uint32_t */
-#define QMI_WDA_DATA_LINK_PROTOCOL_UNKNOWN	0
-#define QMI_WDA_DATA_LINK_PROTOCOL_802_3	1
-#define QMI_WDA_DATA_LINK_PROTOCOL_RAW_IP	2
+
+enum qmi_wda_data_link_protocol {
+	QMI_WDA_DATA_LINK_PROTOCOL_UNKNOWN =			0x00,
+	QMI_WDA_DATA_LINK_PROTOCOL_802_3 =			0x01,
+	QMI_WDA_DATA_LINK_PROTOCOL_RAW_IP =			0x02,
+};
